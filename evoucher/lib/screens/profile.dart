a8242d93a0f678ff3d1cdc9ec68851be5bef2068
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-28 23:06:40 +0000

    Update profile.dart

--- evoucher/lib/screens/profile.dart
+++ evoucher/lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:evoucher/components/btmNavBar.dart';
+import 'package:evoucher/components/profile_item.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -16,6 +17,60 @@ class _ProfileScreenState extends State<ProfileScreen> {
         title: const Text("Profile"),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 0x32, 0xB7, 0x68),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              CircleAvatar(
+                radius: 60,
+                child: Image.asset('assets/images/profile.png'),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Fahd Mohammed",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const Text(
+                "mohammedfahd@gmail.com",
+              ),
+              const SizedBox(height: 20),
+              const PrilfeItem(title: "Profile Settings"),
+              const PrilfeItem(title: "Cash Withdrawal"),
+              const PrilfeItem(title: "Add Funds"),
+              const PrilfeItem(title: "Delete Account"),
+              const Spacer(),
+              SizedBox(
+                width: double.infinity,
+                height: 55,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // _showDialog();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(200, 255, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    "Logout",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: CustomBottomNavBar(selectedIndex: 4),
     );
