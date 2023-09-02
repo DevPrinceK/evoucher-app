0ff53f95d3391aa3d5d19d425c904bff160ad3bd
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-02 12:18:30 +0000

    Update api_endpoints.dart

--- evoucher/lib/network/api_endpoints.dart
+++ evoucher/lib/network/api_endpoints.dart
@@ -1,10 +1,14 @@
 class APIEndpoints {
   // final String baseUrl = "https://api.evoucher.my/api-v1/";
-  static const String baseUrl = "http://192.168.137.1:8000/api-v1/";
+  // static const String baseUrl = "http://192.168.137.1:8000/api-v1/";
+  static const String baseUrl = "http://10.10.18.213:8000/api-v1/";
   static const String login = "${baseUrl}login/";
   static const String logout = "${baseUrl}logout/";
   static const String stats = "${baseUrl}stats/";
   static const String creditWallet = "${baseUrl}credit-wallet/";
   static const String debitWallet = "${baseUrl}debit-wallet/";
   static const String events = "${baseUrl}cud-event/";
+  static const String vouchers = "${baseUrl}cud-voucher/";
+  static const String allEvents = "${baseUrl}all-events/";
+  static const String allVouchers = "${baseUrl}all-vouchers/";
 }
