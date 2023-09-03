cec5490abac6b81e4b4574ed89497e1b6aadd1f0
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-03 21:02:34 +0000

    Update voucher_item.dart

--- evoucher/lib/components/voucher_item.dart
+++ evoucher/lib/components/voucher_item.dart
@@ -7,6 +7,7 @@ class VoucherItemCard extends StatelessWidget {
   final String voucherCreator;
   final String voucherDate;
   final String eventName;
+  final String eventId;
   final String voucherCreatorEmail;
   final double voucherAmount;
   const VoucherItemCard({
@@ -16,6 +17,7 @@ class VoucherItemCard extends StatelessWidget {
     required this.voucherCreator,
     required this.voucherDate,
     required this.eventName,
+    this.eventId = '0',
     this.voucherCreatorEmail = 'vouchercreator@gmail.com',
     this.voucherAmount = 45.00,
   });
@@ -34,6 +36,7 @@ class VoucherItemCard extends StatelessWidget {
               voucherCreator: voucherCreator,
               voucherCreatorEmail: voucherCreatorEmail,
               voucherAmount: voucherAmount,
+              eventID: eventId,
             ),
           ),
         );
