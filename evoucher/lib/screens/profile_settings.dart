d2e65082029a634573917f39d95530e8ab84d131
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-09-03 09:34:40 +0000

    Update profile_settings.dart

--- evoucher/lib/screens/profile_settings.dart
+++ evoucher/lib/screens/profile_settings.dart
@@ -1,10 +1,14 @@
-// ignore_for_file: sized_box_for_whitespace
+// ignore_for_file: sized_box_for_whitespace, avoid_print, use_build_context_synchronously, prefer_final_fields
 
 import 'package:evoucher/components/navbar/app_user_navbar.dart';
 import 'package:evoucher/components/navbar/organizer_nav_bar.dart';
 import 'package:evoucher/components/navbar/restaurant_navbar.dart';
+import 'package:evoucher/network/api_endpoints.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+// import http package and convert dart file
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ProfileSettingsScreen extends StatefulWidget {
   const ProfileSettingsScreen({super.key});
@@ -14,7 +18,16 @@ class ProfileSettingsScreen extends StatefulWidget {
 }
 
 class _ProfileSettingsScreenState extends State<ProfileSettingsScreen> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _fullnameController = TextEditingController();
+
+  // user info
+  String _userEmail = "mohammedfahd@gmail.com";
+  String _userFullname = "Mohammed Fahd";
+
   String userRole = "APP_USER";
+
+  double get deviceWidth => MediaQuery.of(context).size.width;
   // Get the user role from shared preferences
   Future<void> getUserRole() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -27,10 +40,70 @@ class _ProfileSettingsScreenState extends State<ProfileSettingsScreen> {
     print("Role After setState(): $userRole");
   }
 
+  void getProfileInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? email = prefs.getString("email");
+    final String? fullname = prefs.getString("fullname");
+    setState(() {
+      _userEmail = email.toString();
+      _userFullname = fullname.toString();
+
+      _emailController.text = _userEmail;
+      _fullnameController.text = _userFullname;
+    });
+  }
+
+  // add events
+  Future<int> ChangeProfile(fullname, email) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? token = prefs.getString('token');
+
+    var url = Uri.parse(APIEndpoints.changeProfile);
+    var response = await http.post(url, headers: {
+      "Authorization": "Token ${token.toString()}",
+    }, body: {
+      "fullname": fullname,
+      "email": email,
+    });
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      var userData = data["user"];
+      print("Profile Updated Successfully");
+      await prefs.setString("fullname", userData["fullname"]);
+      await prefs.setString("email", userData["email"]);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+    return response.statusCode;
+  }
+
+  // show dialog
+  Future<void> _showSuccessDialog() async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text("Profile Updated!"),
+          content: SizedBox(
+            height: 300,
+            width: deviceWidth * 0.8,
+            child: Column(
+              children: [
+                Image.asset("assets/images/success-check.png"),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     super.initState();
     getUserRole();
+    getProfileInfo();
   }
 
   @override
@@ -55,31 +128,42 @@ class _ProfileSettingsScreenState extends State<ProfileSettingsScreen> {
                 ),
               ),
               const SizedBox(height: 10),
-              const Center(
+              Center(
                 child: Text(
-                  "Fahd Mohammed",
-                  style: TextStyle(
+                  _userFullname,
+                  style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 16,
                   ),
                 ),
               ),
-              const Center(
+              Center(
                 child: Text(
-                  "mohammedfahd@gmail.com",
+                  _userEmail,
+                ),
+              ),
+              Text(
+                userRole,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
                 ),
               ),
               const SizedBox(height: 20),
-              const Column(
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: 20),
-                  Text("Fullname"),
-                  TextField(),
-                  SizedBox(height: 30),
-                  Text("Email"),
-                  TextField(),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
+                  const Text("Fullname"),
+                  TextField(
+                    controller: _fullnameController,
+                  ),
+                  const SizedBox(height: 30),
+                  const Text("Email"),
+                  TextField(
+                    controller: _emailController,
+                  ),
+                  const SizedBox(height: 20),
                 ],
               ),
               const SizedBox(height: 40),
@@ -87,14 +171,30 @@ class _ProfileSettingsScreenState extends State<ProfileSettingsScreen> {
                 width: double.infinity,
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(150, 0, 255, 0),
+                    backgroundColor: const Color.fromARGB(120, 0, 255, 0),
                     foregroundColor: Colors.white,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                   child: const Text("Save Settings"),
-                  onPressed: () {},
+                  onPressed: () async {
+                    print("Email: ${_emailController.text}");
+                    print("Fullname: ${_fullnameController.text}");
+                    var res = await ChangeProfile(
+                        _fullnameController.text, _emailController.text);
+                    if (res == 200) {
+                      _showSuccessDialog();
+                      return;
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Couldn't Update Profile"),
+                        ),
+                      );
+                      return;
+                    }
+                  },
                 ),
               ),
             ],
